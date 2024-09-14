3510dc97e93567899d89a921640b820e4a6b10b2
mj <148041616+yanachified@users.noreply.github.com>
2024-09-14 22:34:37 +0800

    apply changes

--- schema/schemas.py
+++ schema/schemas.py
@@ -4,7 +4,7 @@ def account_serial(account) -> dict:
         "username": str(account["username"]),
         "password": str(account["password"]),
         "created_at": str(account["created_at"]),
-        "updated_at": str(account["created_at"]),
+        "updated_at": str(account["updated_at"]),
     }
 
 def account_list_serial(accounts) -> list:
@@ -52,6 +52,7 @@ def report_serial(report) -> dict:
         "sensor": str(report['sensor']),
         "data": str(report['data']),
         "created_at": str(report['created_at']),
+        "updated_at": str(report['updated_at']),
     }
 
 def report_list_serial(reports) -> list:
