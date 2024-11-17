12748d72b87eac7e3c8981f699009e74620460bf
topecnz <topecnz@gmail.com>
2024-11-18 00:58:58 +0800

    add some routes on account

--- models/account.py
+++ models/account.py
@@ -4,4 +4,23 @@ from datetime import datetime
 class Account(BaseModel):
     username: str
     password: str
+    
+class FirstSetup(BaseModel):
+    id: str
+    password: str
+    security_question: str
+    security_answer: str
+    
+class ResetPassword(BaseModel):
+    id: str
+    password: str
+    
+class UpdateAccount(BaseModel):
+    id: str
+    password: str
+    security_question: str
+    security_answer: str
 
+class SecurityAnswer(BaseModel):
+    id: str    
+    security_answer: str
